c7f4cbf725fa8ea06909ce6d56055fd977fac5cd
Mitch <mjoerussell@gmail.com>
2022-12-23 17:35:07 -0600

    Sign Mode: Implement --append-timestamp, so you can add a timestamp automatically before signing the board

--- src/modes/sign_mode.zig
+++ src/modes/sign_mode.zig
@@ -5,6 +5,7 @@ const Ed25519 = std.crypto.sign.Ed25519;
 const Args = @import("../args.zig").Args;
 const KeyPair = @import("../KeyPair.zig");
 const Board = @import("../Board.zig");
+const Timestamp = @import("../Timestamp.zig");
 
 pub fn run(allocator: Allocator, sign_args: Args.SignArgs) !void {
     const cwd = std.fs.cwd();
@@ -19,14 +20,23 @@ pub fn run(allocator: Allocator, sign_args: Args.SignArgs) !void {
     _ = try key_file.read(&key_bytes_ascii);
 
     var key = try Ed25519.KeyPair.fromSecretKey(try KeyPair.secretKeyFromHexString(&key_bytes_ascii));
-
-    var board_file = try cwd.openFile(sign_args.board, .{});
-    defer board_file.close();
     
-    var board = try board_file.readToEndAlloc(allocator, Board.board_size);
-    defer allocator.free(board);
+    var board_buffer = try std.ArrayList(u8).initCapacity(allocator, Board.board_size);
+    defer board_buffer.deinit();
 
-    var signature = try key.sign(board, null);
+    var board_slice: [Board.board_size]u8 = undefined;
+    const board = try cwd.readFile(sign_args.board, &board_slice);
+    board_buffer.appendSliceAssumeCapacity(board);
+
+    if (sign_args.append_timestamp) {
+        var new_ts = Timestamp.now();
+        try board_buffer.writer().print("<time datetime=\"{}\">", .{new_ts});
+
+        var board_file = try cwd.createFile(sign_args.board, .{});
+        try board_file.writeAll(board_buffer.items);
+    }
+
+    var signature = try key.sign(board_buffer.items, null);
 
     var writer = std.io.getStdOut().writer();
 
